61e59771b102f234d6159ea123569de94d17f1aa
SharkManZ <DG5BZ2TL>
2023-02-25 01:07:28 +0300

    react-scripts 5

--- lego-manager-ui/src/utils/object.utils.js
+++ lego-manager-ui/src/utils/object.utils.js
@@ -3,7 +3,7 @@ export function fetchFromObject(obj, prop) {
         return false;
     }
 
-    var _index = prop.indexOf('.')
+    let _index = prop.indexOf('.')
     if (_index > -1) {
         return fetchFromObject(obj[prop.substring(0, _index)], prop.substr(_index + 1));
     }
